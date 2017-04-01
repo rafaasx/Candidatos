4edda4d748caad976caf93d1dffce822b70e60cb
rafaasx <rafaeltwisted@gmail.com>
2017-04-01 16:07:23 -0300

    My First Commit Yes!

---
+++ WebCandidatos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebCandidatos.MvcApplication" Language="C#" %>
---
+++ WebCandidatos/Js/IndexController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('myApp').controller('IndexController', ['$scope', '$http', function ($scope, $http) {
+        $scope.candidato = {};
+        $scope.enviar = function () {
+            $http.post('/HomeController', JSON.stringify($scope.candidato))
+                .then(function (data) {
+                    console.log("sucesso!");
+                }, function (data) {
+                    console.log("erro!" + JSON.stringify(data));
+                });
+            alert("Enviado");
+        }
+    }]);
+}());
